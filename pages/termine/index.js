fd1850178e19c8ae488018299820283f953307ba
Maria Adner <juergens.maria@googlemail.com>
2024-10-14 10:13:05 +0200

    created termine site

---
+++ pages/termine/index.js
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import SubHeader from "@/components/SubHeader";
+
+export default function Dates() {
+  return (
+    <>
+      <Header />
+      <SubHeader>Termine</SubHeader>
+    </>
+  );
+}
