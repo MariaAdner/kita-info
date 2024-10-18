4342b713be0a2f3c5a088f642e49bca65f6fecce
Maria Adner <juergens.maria@googlemail.com>
2024-10-18 21:26:24 +0200

    added session context

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,25 @@
 import { SWRConfig } from "swr";
+import { SessionProvider } from "next-auth/react";
 import GlobalStyle from "../styles";
 import NavigationBar from "@/components/NavigationBar";
 import Header from "@/components/Header";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <>
-      <GlobalStyle />
-      <SWRConfig value={{ fetcher }}>
-        <Header />
-        <Component {...pageProps} />
-        <NavigationBar />
-      </SWRConfig>
+      <SessionProvider session={session}>
+        <GlobalStyle />
+        <SWRConfig value={{ fetcher }}>
+          <Header />
+          <Component {...pageProps} />
+          <NavigationBar />
+        </SWRConfig>
+      </SessionProvider>
     </>
   );
 }
