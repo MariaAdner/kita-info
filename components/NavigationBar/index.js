56be7d00a3f106f6b2abf57f36ece5935b885362
Maria Adner <juergens.maria@googlemail.com>
2024-10-18 13:06:46 +0200

    added conditional rendering of back button

--- components/NavigationBar/index.js
+++ components/NavigationBar/index.js
@@ -25,23 +25,25 @@ export default function NavigationBar() {
         </StyledNavigationButton>
       </StyledNavigationLink>
       <StyledNavigationLink href="/">
-        <StyledNavigationButton onClick={() => router.back()}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-            width={20}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
-            />
-          </svg>
-        </StyledNavigationButton>
+        {router.pathname !== "/" && (
+          <StyledNavigationButton onClick={() => router.back()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+              width={20}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+              />
+            </svg>
+          </StyledNavigationButton>
+        )}
       </StyledNavigationLink>
       <StyledNavigationLink href="/">
         <StyledNavigationButton>
